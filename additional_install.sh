6ab3bfdd80af7a1f3a617fe585317bf77dfe13de
roman <accounts@romanschloegl.com>
2023-04-30 15:02:39 +0200

    telescope customisation

--- additional_install.sh
+++ additional_install.sh
@@ -1,5 +1,7 @@
 sudo apt install xclip &&
 sudo apt install npm &&
 sudo apt install cargo &&
+sudo apt install ripgrep &&
 mkdir -p ~/.local/share/fonts &&
 curl -fLo ~/.local/share/fonts/Droid\ Sans\ Mono\ for\ Powerline\ Nerd\ Font\ Complete.otf "https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf"
+
