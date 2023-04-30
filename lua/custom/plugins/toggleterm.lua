7edf1832a0186be30855fa163c1b89a599756194
roman <accounts@romanschloegl.com>
2023-04-30 15:55:33 +0200

    replaced floaterm with toggleterm and added linter

---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,21 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = "*",
+  config = function()
+    require('toggleterm').setup {
+      size = 20,
+      open_mapping = [[<F12>]],
+      auto_scroll = true,
+      direction = 'horizontal',
+    }
+
+    local Terminal  = require('toggleterm.terminal').Terminal
+    local lazygit = Terminal:new({ cmd = "lazygit", hidden = true,  direction='float'})
+
+    function _lazygit_toggle()
+      lazygit:toggle()
+    end
+
+    vim.api.nvim_set_keymap("n", "´", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+  end,
+}
