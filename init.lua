1df30caadc286c4b9b6adb3e8d8a5131006bc776
roman <accounts@romanschloegl.com>
2023-04-30 17:03:31 +0200

    show files in neotree that are in gitignore

--- init.lua
+++ init.lua
@@ -235,7 +235,7 @@ vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc =
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').live_grep, { desc = 'Search everywhere (grep)' })
 vim.keymap.set('n', '<leader>b', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>f', require('telescope.builtin').find_files, { desc = 'Find [F]ile' })
-vim.keymap.set('n', '<leader>s', function()
+vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
     winblend = 10,
