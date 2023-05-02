a702b25d15f0d081dd30c56477fd38562fd044fc
re-rosch <roman.schloegl@robotic-eyes.com>
2023-05-02 14:27:55 +0200

    removed another sudo

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage && 
 ./nvim.appimage --appimage-extract &&
 mv squashfs-root / &&
-sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+ln -s /squashfs-root/AppRun /usr/bin/nvim
