917a3385c168a10ab5837b9380a2e9697b3389b9
re-rosch <roman.schloegl@robotic-eyes.com>
2023-05-02 14:39:43 +0200

    added -y otion to apt itsall commands

--- additional_install.sh
+++ additional_install.sh
@@ -1,8 +1,8 @@
 # apt install xclip &&
-apt install npm &&
-apt install cargo &&
-apt install ripgrep &&
-npm install remark &&
+apt install npm -y &&
+apt install cargo -y &&
+apt install ripgrep -y &&
+npm install remark -y &&
 mkdir -p ~/.local/share/fonts &&
 curl -fLo ~/.local/share/fonts/Droid\ Sans\ Mono\ for\ Powerline\ Nerd\ Font\ Complete.otf "https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf"
 
